d395ed9f2c160710a6a38867386956df83bf551e
sudarshanmaan0 <sudarshanmaan0@gmail.com>
2021-06-22 21:29:39 +0530

    made navbar responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarTogglerDemo02')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
+$(function() {
+    $(document).click(function (event) {
+      $('.navbar-collapse').collapse('hide');
+    });
+  });
