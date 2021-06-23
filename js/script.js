c2aa5ae55ba3373072eab344e47c0f72b1214fac
sudarshanmaan0 <sudarshanmaan0@gmail.com>
2021-06-23 14:59:35 +0530

    navbar bug solved

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-$(function() {
-    $(document).click(function (event) {
-      $('.navbar-collapse').collapse('hide');
-    });
-  });
+window.onload = () => {
+    const navLinks = document.querySelectorAll('.nav-link');
+    const navCollapse = document.querySelector('#navbarSupportedContent');
+    navLinks.forEach(c => {
+        c.onclick = () => {
+            if(navCollapse.classList.contains('show')) {
+                navCollapse.classList.remove('show');
+            }
+        }
+    }) 
+}
